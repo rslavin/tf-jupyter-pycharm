921b6c880e5fd7bfeeaddbb7cad5c7d9ec52e479
Rocky <rocky.slavin@gmail.com>
2019-09-23 23:28:59 +0000

    fixed cross-compatibilty issues

--- jupyter.sh
+++ jupyter.sh
@@ -10,8 +10,17 @@ host="somehost.com"
 # note that docker automatically modifies iptables so ufw rules won't affect this
 hostport=8888
 containerport=8888
+apiport=2375 # must be set in /lib/systemd/system/docker.service
 image=tensorflow/tensorflow:latest-gpu-py3-jupyter
 mountdir=$HOME/notebooks # can be overriden with init
+
+func_name(){
+	if [[ -n $BASH_VERSION ]]; then
+		echo "${FUNCNAME[1]}"
+	else
+		echo "${funcstack[@]:1:1}"
+	fi
+}
 
 jc(){
 	case "$1" in
@@ -21,7 +30,7 @@ jc(){
 				echo "Container '$JUPYTER_CONT' already exists. Destroy it? [y|n]"
 				read decision
 				case "$decision" in
-					[yY]) "$0" destroy 2> /dev/null;;
+					[yY]) $(func_name) destroy 2> /dev/null;;
 					*) return
 				esac
 			fi
@@ -29,17 +38,17 @@ jc(){
 			# check for user-specified directory
 			if [[ "$#" > 1 ]]; then
 				if [[ "${2:0:1}" != "/" ]]; then
-					echo "Directory must be specified with absolute path"
-					return
+					mountdir=$(pwd)"/$2"
+				else
+					mountdir="$2"
 				fi
-				mountdir="$2"
 			fi
 			mkdir -p "$mountdir"
 
 			docker run -d --runtime=nvidia -u $(id -u):$(id -g) -v "$mountdir":/tf -it --name $JUPYTER_CONT -p $hostport:$containerport $image > /dev/null \
 				&& echo "Jupyter container '$JUPYTER_CONT' created, mounted directory $mountdir"
 			sleep 4
-			"$0" url;;
+			$(func_name) url;;
 		stop) 
 			docker stop $JUPYTER_CONT > /dev/null && echo "Jupyter container '$JUPYTER_CONT' stopped";;
 		show) 
@@ -49,29 +58,29 @@ jc(){
 			docker ps;;
 		shell) 
 			echo "Use 'exit' to exit"
-			docker container exec -i $JUPYTER_CONT /bin/bash;;
+			docker container exec -u 0 -i $JUPYTER_CONT /bin/bash;;
 		url|token) 
 			docker logs $JUPYTER_CONT | grep '  http.*\?token' | tail -1 | \
 				sed '-e s/^ *//' "-e s/(.\+)/$host/" "-e s/:$containerport/:$hostport/";;
 		start|resume) 
 			docker start $JUPYTER_CONT > /dev/null && sleep 3 \
-				&& echo "Jupyter container '$JUPYTER_CONT' resumed" && "$0" url;;
+				&& echo "Jupyter container '$JUPYTER_CONT' resumed" && $(func_name) url;;
 		destroy) 
-			$0 stop &> /dev/null
+			$(func_name) stop &> /dev/null
 		  	if docker rm $JUPYTER_CONT &> /dev/null; then
 				echo "Jupyter container '$JUPYTER_CONT' destroyed - notebooks preserved"
 			else
-				echo "Unable to destroy container. Use '$0 list' to see if one exists" >&2
+				echo "Unable to destroy container. Use '$(func_name) list' to see if one exists" >&2
 			fi
 			;;
 		log) 
 			docker logs $JUPYTER_CONT;; 
 		*)
 			port=$(sed -n '/tcp:\/\// s/^.*\.0://p' /etc/systemd/system/docker.service.d/override.conf)
-			printf "usage: $0 <command>\n"
+			printf "usage: $(func_name) <command>\n"
 			printf "API daemon running on port $port\n"
 			printf "Permissions problems? Make sure you're in the 'docker' group\n"
-			printf "For first-time setup, run \`$0 init\`. After that, you can use the following arguments:
+			printf "For first-time setup, run \`$(func_name) init\`. After that, you can use the following arguments:
 
 	init [dir]\tCreates the container. If [dir] is passed, your notebooks will be placed in the corresponding directory (MUST BE ABSOLUTE PATH). ~/notebooks is used by default.
 	start\t\tStarts your personal Jupyter container and sends it to the background.
